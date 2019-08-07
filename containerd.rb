f08f387d3a385799febd1f67d855cd859521f290
Hajime Tazaki <thehajime@gmail.com>
2019-08-08 08:52:57 +0900

    fix errors reported by brew audit
    Signed-off-by: Hajime Tazaki <thehajime@gmail.com>

--- containerd.rb
+++ containerd.rb
@@ -1,24 +1,26 @@
-require 'formula'
+HOMEBREW_CTRD_BRANCH="runu-darwin-master-190607".freeze
+HOMEBREW_CTRD_VERSION="beta1".freeze
 
-HOMEBREW_CTRD_BRANCH='runu-darwin-master-190607'
-HOMEBREW_CTRD_VERSION='beta'
 class Containerd < Formula
-  homepage 'https://github.com/ukontainer/containerd'
-  url 'https://github.com/ukontainer/containerd.git', :branch => HOMEBREW_CTRD_BRANCH
+  desc "Container daemon for macOS"
+  homepage "https://github.com/ukontainer/containerd"
+  url "https://github.com/ukontainer/containerd.git", :branch => HOMEBREW_CTRD_BRANCH
   version "#{HOMEBREW_CTRD_BRANCH}-#{HOMEBREW_CTRD_VERSION}"
-  head 'https://github.com/ukontainer/containerd.git', :branch => HOMEBREW_CTRD_BRANCH
+  head "https://github.com/ukontainer/containerd.git", :branch => HOMEBREW_CTRD_BRANCH
 
-  depends_on 'go' => :build
+  depends_on "go" => :build
 
   def install
-    ENV['GOPATH'] = buildpath
-    system 'go', 'get', 'github.com/opencontainers/runtime-spec/specs-go'
-    system 'go', 'get', 'github.com/urfave/cli'
-    system 'go', 'get', 'github.com/sirupsen/logrus'
-    system 'go', 'get', 'github.com/opencontainers/runc/libcontainer/specconv'
-    mkdir_p buildpath/'src/github.com/containerd/'
-    ln_sf buildpath, buildpath/'src/github.com/containerd/containerd'
-    system 'cd src/github.com/containerd/containerd && make'
-    bin.install Dir['src/github.com/containerd/containerd/bin/*']
+    ENV["GOPATH"] = buildpath
+    mkdir_p buildpath/"src/github.com/containerd/"
+    ln_sf buildpath, buildpath/"src/github.com/containerd/containerd"
+    system "cd src/github.com/containerd/containerd && make"
+    bin.install "src/github.com/containerd/containerd/bin/containerd"
+    bin.install "src/github.com/containerd/containerd/bin/containerd-shim"
+    bin.install "src/github.com/containerd/containerd/bin/ctr"
+  end
+
+  test do
+    system "false"
   end
 end
--- dockerd-darwin.rb
+++ dockerd-darwin.rb
@@ -1,22 +1,25 @@
-require 'formula'
+HOMEBREW_DOCKERD_BRANCH="darwin-port".freeze
+HOMEBREW_DOCKERD_VERSION="beta1".freeze
 
-HOMEBREW_DOCKERD_BRANCH='darwin-port'
-HOMEBREW_DOCKERD_VERSION='beta'
 class DockerdDarwin < Formula
-  desc "Moby Project - a collaborative project for the container ecosystem to assemble container-based systems"
+  desc "Docker daemon for macOS"
   homepage "https://github.com/ukontainer/dockerd-darwin"
-  url 'https://github.com/ukontainer/dockerd-darwin.git', :branch => HOMEBREW_DOCKERD_BRANCH
+  url "https://github.com/ukontainer/dockerd-darwin.git", :branch => HOMEBREW_DOCKERD_BRANCH
   version "#{HOMEBREW_DOCKERD_BRANCH}-#{HOMEBREW_DOCKERD_VERSION}"
   head "https://github.com/ukontainer/dockerd-darwin.git", :branch => HOMEBREW_DOCKERD_BRANCH
-  
-  depends_on 'go' => :build
-  depends_on 'ukontainer/lkl/containerd'
+
+  depends_on "go" => :build
+  depends_on "ukontainer/lkl/containerd"
 
   def install
-      ENV['GOPATH'] = buildpath
-      mkdir_p buildpath/'src/github.com/docker/'
-      ln_sf buildpath, buildpath/'src/github.com/docker/docker'
-      system 'cd src/github.com/docker/docker && make -f Makefile.darwin'
-      bin.install Dir['src/github.com/docker/docker/bundles/dynbinary-daemon/*']
+    ENV["GOPATH"] = buildpath
+    mkdir_p buildpath/"src/github.com/docker/"
+    ln_sf buildpath, buildpath/"src/github.com/docker/docker"
+    system "cd src/github.com/docker/docker && make -f Makefile.darwin"
+    bin.install "src/github.com/docker/docker/bundles/dynbinary-daemon/dockerd"
+  end
+
+  test do
+    system "false"
   end
 end
